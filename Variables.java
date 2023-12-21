deb45ae9ed1e0dd26988def64fe51672b1556ede
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2023-12-21 12:33:21 +0530

    commit

---
+++ Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main (String args[]){
+        int a = 10;
+        int b = 5;
+        System.out.println("The value of a is " + a);
+        System.out.println("The value of b is " + b);
+    }
+}
