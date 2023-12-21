272246f77a4c5e9dff81313f76c73e6f77f358ff
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2023-12-21 15:56:50 +0530

    commit

---
+++ Type_casting.java
@@ -0,0 +1,7 @@
+public class Type_casting {
+    public static void main(String[] args) {
+        double d = 46.85;
+        int value_d = (int) d;
+        System.out.println("Integer value of the double is : " + value_d);
+    }
+}
---
+++ Type_conversion.java
@@ -0,0 +1,7 @@
+public class Type_conversion {
+    public static void main(String[] args) {
+    int value = 89665;
+    long long_value = value;
+    System.out.println("long value is : " + long_value);
+    }
+}
