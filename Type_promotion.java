050f01ca59f75a79e000384b80167e7ee4bfd297
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2023-12-21 16:10:28 +0530

    commit

---
+++ Type_promotion.java
@@ -0,0 +1,10 @@
+public class Type_promotion {
+    public static void main(String[] args) {
+        int a = 5;
+        float b = 45.56f;
+        long c = 50;
+        double d = 56.7854;
+        double sum = a + b + c + d;
+        System.out.println("The sum will be : " + sum);
+    }
+}
