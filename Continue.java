25d1ef63a02ddc1090a69d01d4727eabe7673e6c
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2023-12-27 14:39:28 +0530

    modify

--- Continue.java
+++ Continue.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Continue {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        do {
-            System.out.println("Enter your number : ");
-            int n = scan.nextInt();
-            if (n % 10 == 0) {
-                continue;
-            }
-            System.out.println("The entered number is : " + n);
-        } while (true);
+        try (Scanner scan = new Scanner(System.in)) {
+            do {
+                System.out.println("Enter your number : ");
+                int n = scan.nextInt();
+                if (n % 10 == 0) {
+                    continue;
+                }
+                System.out.println("The entered number is : " + n);
+            } while (true);
+        }
     }
 }
