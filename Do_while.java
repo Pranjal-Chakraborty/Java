50369576dc48b053ae485380c9eeefcec368b298
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2023-12-27 14:10:33 +0530

    commit

---
+++ Do_while.java
@@ -0,0 +1,9 @@
+public class Do_while {
+    public static void main(String[] args) {
+        int count = 1;
+        do{
+            System.out.println("Hello World!");
+            count++;
+        }while(count <= 1);
+    }
+}   
