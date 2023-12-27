e323682d5034cee675846c44ebd150aa17249258
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2023-12-27 14:38:42 +0530

    commit

---
+++ Continue.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Continue {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        do {
+            System.out.println("Enter your number : ");
+            int n = scan.nextInt();
+            if (n % 10 == 0) {
+                continue;
+            }
+            System.out.println("The entered number is : " + n);
+        } while (true);
+    }
+}
