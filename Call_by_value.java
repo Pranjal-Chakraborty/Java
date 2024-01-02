944b0d60898460d5a47fb0bf5e0b90ee8347e5f6
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2024-01-02 17:09:05 +0530

    commit

---
+++ Call_by_value.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Call_by_value {
+
+    public static void square(int n){
+        n *= n;
+        System.out.println("The output inside the square function is : " + n);
+    }
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num = scan.nextInt();
+        scan.close();
+        
+        System.out.println("Before calling the function the number is : " + num);
+        
+        square(num);
+
+        System.out.println("After calling of the square function the value of the number is : "+ num);
+    }
+}
