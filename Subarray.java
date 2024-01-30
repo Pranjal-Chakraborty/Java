0821462ce8f2ceb83ae90bc4419104c7f63f1bbc
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2024-01-30 14:00:37 +0530

    Fix subarray iteration bug

--- Subarray.java
+++ Subarray.java
@@ -4,11 +4,12 @@ public class Subarray {
     public static void subarrays(int arr[]){
         for(int i = 0; i < arr.length ; i++){
             for(int j = i; j < arr.length ; j++){
-                for(int k = i; k < j ; k++){
+                for(int k = i; k <= j ; k++){
                     System.out.print(arr[k] + " ");
                 }
                 System.out.println();
             }
+        System.out.println();
         }
     }
     public static void main(String[] args) {
