275207428fe4a9fa8755647d7b59a568e4770bec
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2024-02-05 12:22:37 +0530

    Refactor main method indentation

--- Stocks.java
+++ Stocks.java
@@ -16,7 +16,7 @@ public class Stocks {
         }
         return maxprofit;
     }
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the number of days : ");
         int len = scan.nextInt();
