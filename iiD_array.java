dda05f190a180f48415fefd3b810e5889a9f45d6
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2024-02-06 13:55:10 +0530

    Add iiD_array.java - a program to input and display marks of students in a 2D array

---
+++ iiD_array.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class iiD_array {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number od students : ");
+        int m = scan.nextInt();
+        System.out.println("Enter the number of the subjects : ");
+        int n = scan.nextInt();
+
+        int marks[][] = new int[m][n];
+        for(int i = 0; i < m; i++){
+            for(int j =0; j < n; j++){
+                System.out.print("Enter Student " + (i+1) + " subject " + (j+1) + " marks : ");
+                marks[i][j] = scan.nextInt();
+            }
+        }
+        scan.close();
+
+        System.out.println();
+
+        for(int i = 0; i < m; i++){
+            for(int j =0; j < n; j++){
+                System.out.print(marks[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
