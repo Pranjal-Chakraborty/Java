c62f9666e3f9e63a4804ee72cc12eedff61f65f6
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2024-04-01 12:39:09 +0530

    Add Strings.java with basic string operations

---
+++ Strings.java
@@ -0,0 +1,19 @@
+public class Strings {
+    
+    public static void printletters(String str){
+        for(int i = 0; i < str.length(); i++){
+            System.out.print(str.charAt(i) + " ");
+        }
+        System.out.println();
+    }
+    public static void main (String args[]){
+        String firstname = "Pranjal";
+        String lastname = "Chakraborty";
+        String fullname = firstname + " " + lastname;
+        System.out.println(fullname);
+        System.out.println("The hashcode for first name is : " + firstname.hashCode());
+        System.out.println("The hashcode for last name is : " + lastname.hashCode());
+        System.out.println("The hashcode for full name is : " + firstname.hashCode());
+        printletters(fullname);
+    }
+}
