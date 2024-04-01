57a00c0b38231919cfc5c92b72bb710b01bb10a7
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2024-04-01 15:10:12 +0530

    Refactor Substring.java to include custom substring method

--- Substring.java
+++ Substring.java
@@ -1,6 +1,19 @@
-public class Substring {
+public class Substring { 
     public static void main(String[] args) {
         String str = "HelloWorld";
         System.out.println(str.substring(3,6));
     }
+    /* If we want to do the same using our own method the code will be.
+
+    public static void substr(String str, int startindex, int endindex){
+        for(int i = startindex; i < endindex ; i++){
+            System.out.print(str.charAt(i));
+        }
+    }
+    
+    public static void main(String[] args) {
+        String str = "HelloWorld";
+        substr(str,3,6));
+    }
+    */ 
 }
