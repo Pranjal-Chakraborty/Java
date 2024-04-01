31bbaf7f8f1cce65aa2b945d64e951db6fec0905
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2024-04-02 00:54:26 +0530

    Add String_compress.java with string compression implementation

---
+++ String_compress.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class String_compress {
+    public static String compressString(String str){
+        StringBuffer newstr = new StringBuffer("");
+        for(int i = 0; i < str.length(); i++){
+            Integer count = 1;
+            while(i < str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            newstr.append(str.charAt(i));
+            if(count > 1){
+                newstr.append(count.toString());
+            }
+        }
+        return newstr.toString();
+    }
+    public static void main(String[] args) {
+        String str = new String();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a string : ");
+        str = scan.next();
+        scan.close();
+        System.out.println("The compressed string will be : " + compressString(str));
+    }
+}
