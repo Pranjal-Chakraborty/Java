8bddd99bcfb4f4a45b6752dd1dbfdc95bcbc5954
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2024-04-03 16:34:19 +0530

    Refactor Bitclear.java to use clearbit instead of setbit

--- Bitclear.java
+++ Bitclear.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Bitclear {
-    public static int setbit(int n, int i){
+    public static int clearbit(int n, int i){
         int bitmask = ~(1 << i);
         System.out.println("Before changes the number is : " + n);
         return (n & bitmask);
@@ -13,7 +13,7 @@ public class Bitclear {
         System.out.println("Enter the position of the bit you want to set : ");
         int position = scan.nextInt();
         scan.close();
-        int result = setbit(number , position);
+        int result = clearbit(number , position);
         System.out.println("After changes the number is : " + result);
     }
 }
