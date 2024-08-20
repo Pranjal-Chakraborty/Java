297aa172d181739858b05e25e5bdcf647e76c359
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2024-08-20 15:04:30 +0530

    feat: Add Fibonacci_Recursive class with recursive Fibonacci series calculation

---
+++ Fibonacci_Recursive.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Fibonacci_Recursive {
+      
+    public static int fibonacci(int n){
+        if(n == 0 || n == 1){
+            return n;
+        }
+        int fib = fibonacci(n-1) + fibonacci(n-2);
+        return fib;
+    }
+
+    public static void main(String args[]){
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the range : ");
+        int num = scan.nextInt();
+        scan.close();
+        System.out.println("The fibonacci series of " + num + " numbers is : ");
+        for(int i = 0; i < num; i++){
+            System.out.print(fibonacci(i)+ " ");
+        }
+    }
+}
