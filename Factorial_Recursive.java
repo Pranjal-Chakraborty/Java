d8e68ca44f5d8577de12d4dfad1f5efcfac26168
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2024-08-20 13:05:28 +0530

    feat: Add Factorial_Recursive class with recursive factorial calculation

---
+++ Factorial_Recursive.java
@@ -0,0 +1,22 @@
+import java. util.*;
+
+public class Factorial_Recursive {
+    
+    public static int factorial(int n){
+        if(n == 0){
+            return 1;
+        }
+        return n*factorial(n-1);
+    }
+
+    public static void main(String args[]){
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num = scan.nextInt();
+        scan.close();
+
+        int fact = factorial(num);
+        System.out.println("The factorial of the given number is : " + fact);
+    }
+}
