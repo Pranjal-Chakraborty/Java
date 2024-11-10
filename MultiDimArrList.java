57522b6a2e05d51684447d993a7d4abd639049d1
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2024-11-10 20:46:19 +0530

    Refactor MultiDimArrList to initialize lists using loops for better scalability

--- MultiDimArrList.java
+++ MultiDimArrList.java
@@ -5,17 +5,17 @@ public class MultiDimArrList {
         ArrayList<ArrayList<Integer>> mainList = new ArrayList<>();
         
         ArrayList<Integer> list1 = new ArrayList<>();
-        list1.add(1); list1.add(2); list1.add(3); list1.add(4); list1.add(5);
-        mainList.add(list1);
-
         ArrayList<Integer> list2 = new ArrayList<>();
-        list2.add(2); list2.add(4); list2.add(6); list2.add(8); list2.add(10);
-        mainList.add(list2);
-
         ArrayList<Integer> list3 = new ArrayList<>();
-        list3.add(3); list3.add(6); list3.add(9); list3.add(12); list3.add(15);
+        for(int i = 1; i <= 5; i++){
+            list1.add(i * 1);
+            list2.add(i * 2);
+            list3.add(i * 3);
+        }
+        mainList.add(list1);
+        mainList.add(list2);
         mainList.add(list3);
-
+        
         for(int i = 0; i < mainList.size(); i++){
             ArrayList<Integer> currentList = mainList.get(i);
             System.out.print("The elements of list " + (i + 1) + " are : ");
