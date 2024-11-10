69c38fcb8d70cb3e205fd32cc08d3b357b8c4d3b
Pranjal-Chakraborty <pranjalchakraborty2003@gmail.com>
2024-11-10 17:52:45 +0530

    Add ArrListSort class to demonstrate in-built sorting of an ArrayList in Java

---
+++ ArrListSort.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrListSort {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+
+        System.out.println("Before sorting the arraylist is : " + list);
+        Collections.sort(list);
+        System.out.println("After sorting the arraylist is : " + list);
+    }
+}
